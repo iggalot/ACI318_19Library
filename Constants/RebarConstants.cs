c75619f629763e888d9cbdfa0c9bbed703f05639
Jim <iggalot.iggy@gmail.com>
2021-09-27 08:53:30 -0500

    Initial commit of library files

---
+++ Constants/RebarConstants.cs
@@ -0,0 +1,27 @@
+﻿namespace ACI319_19Library.Constants
+{
+    public static class RebarConstants
+    {
+        /// <summary>
+        /// Helper class to hold standard ACI bar sizes.
+        /// </summary>
+        public static class RebarData
+        {
+            /// <summary>
+            /// ACI318_19 Rebar Constants in order 3, 4, 5 ,6 7 , 8 ,9 10, 11, 14, 18
+            /// </summary>
+            ///                           
+            
+            // Rebar numeric sizes
+            public static int[] Sizes =       {    3,       4,       5,       6,       7,       8,       9,      10,      11,      14,      18 };
+
+            // Rebar cross sectional areas in sq, in.
+            public static double[] Areas =   {  0.11,    0.20,    0.31,    0.44,    0.60,    0.79,    1.00,    1.27,    1.56,    2.25,    4.00 };
+
+            // Rebar weight per foot in plf
+            public static double[] Weights = { 0.376,   0.668,   1.043,   1.502,   2.044,   2.670,   3.400,   4.303,   5.313,   7.650,   13.60 };
+        }
+
+
+    }
+}
---
+++ CoverAndSpacing/CoverAndspacing.cs
@@ -0,0 +1,137 @@
+﻿namespace ACI318_19Library
+{
+    /// <summary>
+    /// Library functions for ACI318_19 provisions
+    /// </summary>
+    public static class ACI318_19_CoverAndspacing
+    {
+
+        public enum MemberTypes
+        {
+            MEMBER_TYPE_SLAB = 0,
+            MEMBER_TYPE_JOIST = 1,
+            MEMBER_TYPE_WALL = 2,
+            MEMBER_TYPE_BEAM = 3,
+            MEMBER_TYPE_COLUMN = 4,
+            MEMBER_TYPE_TENSIONTIE = 5,
+            MEMBER_TYPE_STRUT = 6,
+            MEMBER_TYPE_PEDASTAL = 7,
+            MEMBER_TYPE_WALL_BOUNDARYELEMENT = 8
+        }
+
+        public enum ReinforcingTypes
+        {
+            REINF_TYPE_LONGITUDINAL = 0,
+            REINF_TYPE_STIRRUP = 1,
+            REINF_TYPE_TIE = 2,
+            REINF_TYPE_SPIRALS = 3,
+            REINF_TYPE_HOOP = 4
+        }
+
+        /// <summary>
+        /// Computes the minimum cover for CIP members per Table 20.5.1.3.2
+        /// </summary>
+        /// <param name="bar_size">rebar size #</param>
+        /// <param name="is_footing">member is cast against and permanently in contact with ground</param>
+        /// <param name="is_exposed_weather">member is exposed to weather or in contact with ground</param>
+        /// <param name="mem_type">type of member <see cref="MemberTypes"/></param>
+        /// <returns></returns>
+        public static double ACI318_19_MinCover_CIP(double bar_size, bool is_footing, bool is_exposed_weather, MemberTypes mem_type, ReinforcingTypes reinf_type, out string msg)
+        {
+            if (is_footing)
+            {
+                msg = "3.0 for footings per ACI Table 20.5.1.3.2";
+                return 3.0;
+            }
+
+            if (is_exposed_weather)
+            {
+                if (mem_type == MemberTypes.MEMBER_TYPE_SLAB || mem_type == MemberTypes.MEMBER_TYPE_JOIST || mem_type == MemberTypes.MEMBER_TYPE_WALL || mem_type == MemberTypes.MEMBER_TYPE_WALL_BOUNDARYELEMENT)
+                {
+                    msg = "1.0 for slabs, joists and walls exposed to weather per ACI Table 20.5.1.3.2";
+                    return 1.0;
+                }
+                else
+                {
+                    msg = "1.5 for all others exposed to weather per ACI Table 20.5.1.3.2";
+                    return 1.5;
+                }
+            }
+            else
+            {
+                if (mem_type == MemberTypes.MEMBER_TYPE_SLAB || mem_type == MemberTypes.MEMBER_TYPE_JOIST || mem_type == MemberTypes.MEMBER_TYPE_WALL || mem_type == MemberTypes.MEMBER_TYPE_WALL_BOUNDARYELEMENT)
+                {
+                    msg = "0.75 for slabs, joists and walls not exposed to weather per ACI Table 20.5.1.3.2";
+                    return 0.75;
+                }
+                else
+                {
+                    msg = "0.75 for beams columns and tension ties ";
+
+                    if (reinf_type == ReinforcingTypes.REINF_TYPE_LONGITUDINAL)
+                    {
+                        msg += "0.75 for primiary reinf. in beams columns and tension ties per ACI Table 20.5.1.3.2";
+                        return 1.5;
+                    }
+                    else
+                    {
+                        msg += "1.0 for stirrups, ties, and spirals in beams columns and tension ties per ACI Table 20.5.1.3.2";
+                        return 1.0;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the minimum bar spacing per Section 25.2
+        /// </summary>
+        /// <param name="bar_size">rebar size #</param>
+        /// <param name="agg_dia">max aggregate size</param>
+        /// <returns>minimum horizontal clear spacing</returns>
+        public static double ACI318_19_25_2_MinHorizontalClearSpacing(double bar_size, double agg_dia, ReinforcingTypes reinf_type, MemberTypes memb_type, out string msg)
+        {
+            if (reinf_type == ReinforcingTypes.REINF_TYPE_LONGITUDINAL)
+            {
+                // ACI 25.2.3 for longitudinal steel in columns, pedastals, struts, boundary element
+                if ((memb_type == MemberTypes.MEMBER_TYPE_COLUMN) || (memb_type == MemberTypes.MEMBER_TYPE_PEDASTAL) || (memb_type == MemberTypes.MEMBER_TYPE_COLUMN) || (memb_type == MemberTypes.MEMBER_TYPE_WALL_BOUNDARYELEMENT))
+                {
+                    double max = 1.5;
+                    max = (1.5 * bar_size / 8.0 > max) ? 1.5 * bar_size / 8.0 : max;
+                    max = (max > 1.33 * agg_dia) ? max : 1.33 * agg_dia;
+                    msg = "Min. horizontal clear spacing = " + max.ToString() + " per 25.2.3";
+                    return max;
+                }
+                else
+                {
+                    // ACI 25.2.1
+                    double max = 1.0;
+                    max = (bar_size / 8.0 > max) ? bar_size / 8.0 : max;
+                    max = (max > 1.33 * agg_dia) ? max : 1.33 * agg_dia;
+                    msg = "Min. horizontal clear spacing = " + max.ToString() + " per 25.2.1";
+                    return max;
+                }
+            }
+
+            // TODO:: Our catch all case.  Needs further expansion for prestressed and others ACI 25.2. Currently usues 25.2.1 as default
+            double max_default = 1.5;
+            max_default = (1.5 * bar_size / 8.0 > max_default) ? 1.5 * bar_size / 8.0 : max_default;
+            max_default = (max_default > 1.33 * agg_dia) ? max_default : 1.33 * agg_dia;
+            msg = "Min. horizontal clear spacing = " + max_default.ToString() + " per 25.2.3";
+            return max_default;
+        }
+
+        /// <summary>
+        /// Returns the minimum vertical bar spacing per Section 25.2
+        /// </summary>
+        /// <param name="bar_size">rebar size #</param>
+        /// <param name="agg_dia">max aggregate size</param>
+        /// <returns>minimum vertical clear spacing</returns>
+        public static double ACI318_19_25_2_MinVerticalClearSpacing(int num_layers, out string msg)
+        {
+            // ACI 25.2.2
+            msg = "Min. vert. spacing = 1.0 per 25.2.2";
+            return 1.0;
+        }
+
+    }
+}
